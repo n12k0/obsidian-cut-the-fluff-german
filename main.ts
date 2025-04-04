0e094fa9ae47ded8c412803f08ed6788e97e7441
Adam Fletcher <signup@adamfletcher.com>
2025-04-04 15:16:30 +0100

    Initial version

--- main.ts
+++ main.ts
@@ -1,85 +1,99 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	ViewUpdate,
+	PluginValue,
+	EditorView,
+	ViewPlugin,
+	Decoration,
+	DecorationSet
+} from '@codemirror/view';
+import { RangeSetBuilder } from '@codemirror/state';
+import { syntaxTree } from '@codemirror/language';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface CutTheFluffPluginSettings {
 	mySetting: string;
+	xsColor: string,
+	smColor: string;
+	mdColor: string,
+	lgColor: string,
+	xlColor: string,
+	enabled: boolean,
+	showInRibbon: boolean,
+	xsThreshold: number,
+	smThreshold: number,
+	mdThreshold: number,
+	lgThreshold: number,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CutTheFluffPluginSettings = {
+	mySetting: 'default',
+	xsColor: '#fff2c8',
+	smColor: '#eadbf6',
+	mdColor: '#c5f2cd',
+	lgColor: '#f9caca',
+	xlColor: '#d1f6f4',
+	enabled: false,
+	showInRibbon: false,
+	xsThreshold: 2,
+	smThreshold: 5,
+	mdThreshold: 10,
+	lgThreshold: 20,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CutTheFluffPlugin extends Plugin {
+	settings: CutTheFluffPluginSettings;
+	forceViewUpdate: boolean;
+	regex: RegExp;
 
 	async onload() {
 		await this.loadSettings();
+		this.buildRegex();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'toggle',
+			name: 'Toggle highlighting',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.settings.enabled = !this.settings.enabled;
+				this.saveSettings();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new CutTheFluggSettingsTab(this.app, this));
+		this.registerEditorExtension(this.createViewPlugin());
+		this.updateStyles();
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	updateStyles() {
+		document.documentElement.style.setProperty("--sentence-length-highlight-color-xs", this.settings.xsColor);
+		document.documentElement.style.setProperty("--sentence-length-highlight-color-sm", this.settings.smColor);
+		document.documentElement.style.setProperty("--sentence-length-highlight-color-md", this.settings.mdColor);
+		document.documentElement.style.setProperty("--sentence-length-highlight-color-lg", this.settings.lgColor);
+		document.documentElement.style.setProperty("--sentence-length-highlight-color-xl", this.settings.xlColor);
 	}
 
 	onunload() {
 
+	}
+
+	buildRegex() {
+		const words = [
+			'basically',
+			'just',
+			'basically',
+			'actually',
+			'actual',
+			'literally',
+			'really',
+			'very',
+			'quite',
+			'honestly',
+			'simply',
+			'like',
+			'combine together'
+		];
+		
+		const pattern = `\\b(?:${ words.join("|") })\\b`
+		this.regex = new RegExp(pattern, 'gi');
 	}
 
 	async loadSettings() {
@@ -88,47 +102,169 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		this.forceViewUpdate = true;
+		document.body.classList.toggle('sentence-length-highlighting-active', this.settings.enabled);
+		this.app.workspace.updateOptions();
+	}
+
+	createViewPlugin() {
+		const plugin = this;
+
+		class CutTheFluffViewPlugin implements PluginValue {
+			decorations: DecorationSet;
+
+			constructor(view: EditorView) {
+				this.decorations = this.buildDecorations(view);
+			}
+
+			update(update: ViewUpdate) {
+				if (update.docChanged || update.viewportChanged || plugin.forceViewUpdate) {
+					this.decorations = this.buildDecorations(update.view);
+				}
+			}
+
+			buildDecorations(view: EditorView): DecorationSet {
+
+				if (!plugin.settings.enabled) {
+					return Decoration.none;
+				}
+
+				const builder = new RangeSetBuilder<Decoration>();
+				const text = view.state.doc.toString();
+
+				// Skip highlighting inside code blocks, comments, etc.
+
+				interface NumberRange {
+					min: number;
+					max: number;
+				}
+				const skipRanges: NumberRange[] = [];
+
+				for (let { from, to } of view.visibleRanges) {
+					syntaxTree(view.state).iterate({
+						from,
+						to,
+						enter(node) {
+							if ((node.name.includes("code") || node.name.includes("comment") || node.name.includes("link") || node.name.includes("url"))) {
+								skipRanges.push({ min: node.from, max: node.to });
+							}
+						},
+					});
+				}
+
+				//const words = ["just", "sample", "is"];
+
+				//const sentenceRegex = /\bbasically\b|\btext\b/gi
+				
+				let match;
+
+				for (const { from, to } of view.visibleRanges) {
+					
+
+					// IMPORTANT: Reset lastIndex for global regexes when searching new strings/slices
+					plugin.regex.lastIndex = 0;
+
+					const doc = view.state.doc
+
+					const visibleText = doc.sliceString(from, to);
+
+					while ((match = plugin.regex.exec(visibleText)) !== null) {
+
+						//console.log(text.length);
+
+						let start: number;
+						
+							start = match.index + from;
+						
+						const end = start + match[0].length;
+	
+						if (skipRanges.some(range => start >= range.min && start <= range.max)) {
+							continue;
+						}
+	
+						
+						
+	
+						
+	
+	
+						builder.add(start, end, Decoration.mark({
+							class: 'fluff',
+						}));
+					}
+
+
+				}
+
+				/*
+				while ((match = sentenceRegex.exec(text)) !== null) {
+
+					let start: number;
+					
+						start = match.index;
+					
+					const end = start + match[0].length;
+
+					if (skipRanges.some(range => start >= range.min && start <= range.max)) {
+						continue;
+					}
+
+					
+					
+
+					
+
+
+					builder.add(start, end, Decoration.mark({
+						class: 'fluff',
+					}));
+				}
+				*/
+
+				return builder.finish();
+			}
+		}
+
+		return ViewPlugin.fromClass(CutTheFluffViewPlugin, {
+			decorations: (value: CutTheFluffViewPlugin) => value.decorations,
+		});
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CutTheFluggSettingsTab extends PluginSettingTab {
+	plugin: CutTheFluffPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CutTheFluffPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+			.setName('Enable highlighting')
+			.setDesc('You can also toggle on and off from the command palette')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enabled) // Set the initial state of the toggle from loaded settings
+				.onChange(async (value) => { // This function runs whenever the toggle is changed
+					this.plugin.settings.enabled = value;
 					await this.plugin.saveSettings();
 				}));
+
+
+
+		
+
+
+
+
 	}
 }
+
+
+
+
+
